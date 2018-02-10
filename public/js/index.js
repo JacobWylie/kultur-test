213d0337f17249158b51098e486dc29b3d4a162b
JacobWylie <jacobwylie@gmail.com>
2018-02-10 18:04:07 +0100

    animate dropdown menu and style that baby

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,11 @@
 const hamburger = document.querySelector('.hamburger');
+const dropdownNav = document.querySelector('.dropdown-nav');
 
 // Functionality for burger menu
 function burgerMenu() {
 	// Adds/Removes class from burger for css animation
 	this.classList.toggle('is-active');
+	dropdownNav.classList.toggle('fade')
 }
 
 hamburger.addEventListener('click', burgerMenu);
